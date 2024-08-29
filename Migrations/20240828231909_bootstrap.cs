088e994f9958cc673da2134f624ce55e846b23f6
Karol Silva Guimarães <karol.guimaraes@aluno.ifsp.edu.br>
2024-08-28 21:18:01 -0300

    Remocao da coluna de resposta na view de index

--- Migrations/20240828231909_bootstrap.cs
+++ Migrations/20240828231909_bootstrap.cs
@@ -5,7 +5,7 @@
 namespace Piadas.Migrations
 {
     /// <inheritdoc />
-    public partial class Iníciodaaplicação : Migration
+    public partial class bootstrap : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
